e59c299e75471665dea694811842466498516587
Matheus Lopes Santos <informatica.fadenor@unimontes.br>
2018-09-18 15:36:07 -0300

    Refatoração da classe XML parser, que agora realiza o agrupamento de informações

--- src/XML/XMLParser.php
+++ src/XML/XMLParser.php
@@ -2,10 +2,24 @@
 
 namespace Boleto\Caixa\XML;
 
+use Exception;
 use SimpleXMLElement;
 
+/**
+ * XMLParser
+ *
+ * @author  Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @since   18/09/2018
+ * @package Boleto\Caixa\XML
+ */
 class XMLParser
 {
+    /**
+     * @param   string  $xml
+     * @return  \stdClass
+     * @throws  Exception
+     */
     public static function parseFromRetorno($xml)
     {
         $clean_xml  = str_ireplace([
@@ -17,8 +31,20 @@ class XMLParser
         ], '', $xml);
 
         $parsed = new SimpleXMLElement($clean_xml);
-//        $parsed = simplexml_load_string($clean_xml);
 
-        return $parsed->Body->SERVICO_SAIDA->DADOS;
+        $dados = $parsed->Body->SERVICO_SAIDA->DADOS;
+
+        if ( $dados->CONTROLE_NEGOCIAL->COD_RETORNO == 1 ) {
+            throw new Exception($dados->CONTROLE_NEGOCIAL->MENSAGENS->RETORNO);
+        } else {
+            $boleto = new \stdClass();
+
+            $boleto->codigoBarras   = $dados->INCLUI_BOLETO->CODIGO_BARRAS[0];
+            $boleto->linhaDigitavel = $dados->INCLUI_BOLETO->LINHA_DIGITAVEL[0];
+            $boleto->nossoNumero    = $dados->INCLUI_BOLETO->NOSSO_NUMERO[0];
+            $boleto->urlBoleto      = $dados->INCLUI_BOLETO->URL[0];
+
+            return $boleto;
+        }
     }
 }
